cb56f43b1a01d3601dd92026d6abc0607712d5f5
Arpan <gargarpan98@gmail.com>
2025-01-25 09:28:32 +0530

    simple switch case

--- src/calculator.java
+++ src/calculator.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
+
 public class calculator {
     public static void main(String[] args) {
         System.out.println("Arpan");
+        System.out.println("Choose an operator to perform calculation + , - , / , * , % ");
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+
+        char ch = input.charAt(0);
+
+        switch (ch){
+            case '+':
+                System.out.println("Addition");
+                break;
+                case '-':
+                    System.out.println("Subtraction");
+                    break;
+                    case '*':
+                        System.out.println("Multiplication");
+                        break;
+                        case '/':
+                            System.out.println("Division");
+                            break;
+                            default:
+                                System.out.println("Invalid input");
+                                break;
+
+
+        }
 
     }
 }
